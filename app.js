fe485ce44ff5832fbae30def73ef366f2be4d190
fahimnuman <fahiomnuman87@gmail.com>
2023-05-20 21:47:39 +0600

    add home page

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
 //Load animation if fields containing data on page load
 $( document ).ready(function() {
-    $(".input-login").each(function() { 
-      if ($(this).val() != "") {
-        $(this).parent().addClass("animation");
-      }
-    });
+  $(".app_date").kendoDatePicker();
+   
   });
   
-  //Add animation when input is focused
-  $(".login-input").focus(function(){
-    $(this).parent().addClass("animation animation-color");
-  });
-  
-  //Remove animation(s) when input is no longer focused
-  $(".login-input").focusout(function(){
-    if($(this).val() === "")
-      $(this).parent().removeClass("animation");
-    $(this).parent().removeClass("animation-color");
-  })
